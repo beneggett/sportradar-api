323a84fad147641790dcb6bb91475291ad43a918
Ben Eggett <beneggett@gmail.com>
2016-09-28 16:35:12 -0600

    Starts adding tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'sportradar/api'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'minitest/spec'
 require 'minitest/focus'
 require 'webmock/minitest'
 require 'vcr'
@@ -23,37 +24,37 @@ VCR.configure do |c|
   c.cassette_library_dir = "test/vcr/cassettes"
   c.hook_into :webmock
   c.ignore_hosts 'codeclimate.com'
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:cricket ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:esports_csgo ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:golf ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:mlb ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:mma ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:nascar ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:nba ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:ncaaf ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:ncaamb ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:ncaawb ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:nfl ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:nhl ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:npb ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:rugby ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:soccer_africa ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:soccer_asia ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:soccer_europe ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:soccer_na ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:soccer_sa ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:soccer_wc ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:tennis ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:wnba ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:images_nfl ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:images_nba ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:images_mls ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:live_images_nfl ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:live_images_nba ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:live_images_mls ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:content_nba_v2 ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:content_nfl_v2 ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:content_nba ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:content_nfl ) }
-  c.filter_sensitive_data('<API_KEY>')  { api_key(:odds ) }
+  c.filter_sensitive_data('<CRICKET_API_KEY>')  { api_key(:cricket ) }
+  c.filter_sensitive_data('<ESPORTS_CSGO_API_KEY>')  { api_key(:esports_csgo ) }
+  c.filter_sensitive_data('<GOLF_API_KEY>')  { api_key(:golf ) }
+  c.filter_sensitive_data('<MLB_API_KEY>')  { api_key(:mlb ) }
+  c.filter_sensitive_data('<MMA_API_KEY>')  { api_key(:mma ) }
+  c.filter_sensitive_data('<NASCAR_API_KEY>')  { api_key(:nascar ) }
+  c.filter_sensitive_data('<NBA_API_KEY>')  { api_key(:nba ) }
+  c.filter_sensitive_data('<NCAAF_API_KEY>')  { api_key(:ncaaf ) }
+  c.filter_sensitive_data('<NCAAMB_API_KEY>')  { api_key(:ncaamb ) }
+  c.filter_sensitive_data('<NCAAWB_API_KEY>')  { api_key(:ncaawb ) }
+  c.filter_sensitive_data('<NFL_API_KEY>')  { api_key(:nfl ) }
+  c.filter_sensitive_data('<NHL_API_KEY>')  { api_key(:nhl ) }
+  c.filter_sensitive_data('<NPB_API_KEY>')  { api_key(:npb ) }
+  c.filter_sensitive_data('<RUGBY_API_KEY>')  { api_key(:rugby ) }
+  c.filter_sensitive_data('<SOCCER_AFRICA_API_KEY>')  { api_key(:soccer_africa ) }
+  c.filter_sensitive_data('<SOCCER_ASIA_API_KEY>')  { api_key(:soccer_asia ) }
+  c.filter_sensitive_data('<SOCCER_EUROPE_API_KEY>')  { api_key(:soccer_europe ) }
+  c.filter_sensitive_data('<SOCCER_NA_API_KEY>')  { api_key(:soccer_na ) }
+  c.filter_sensitive_data('<SOCCER_SA_API_KEY>')  { api_key(:soccer_sa ) }
+  c.filter_sensitive_data('<SOCCER_WC_API_KEY>')  { api_key(:soccer_wc ) }
+  c.filter_sensitive_data('<TENNIS_API_KEY>')  { api_key(:tennis ) }
+  c.filter_sensitive_data('<WNBA_API_KEY>')  { api_key(:wnba ) }
+  c.filter_sensitive_data('<IMAGES_NFL_API_KEY>')  { api_key(:images_nfl ) }
+  c.filter_sensitive_data('<IMAGES_NBA_API_KEY>')  { api_key(:images_nba ) }
+  c.filter_sensitive_data('<IMAGES_MLS_API_KEY>')  { api_key(:images_mls ) }
+  c.filter_sensitive_data('<LIVE_IMAGES_NFL_API_KEY>')  { api_key(:live_images_nfl ) }
+  c.filter_sensitive_data('<LIVE_IMAGES_NBA_API_KEY>')  { api_key(:live_images_nba ) }
+  c.filter_sensitive_data('<LIVE_IMAGES_MLS_API_KEY>')  { api_key(:live_images_mls ) }
+  c.filter_sensitive_data('<CONTENT_NBA_V2_API_KEY>')  { api_key(:content_nba_v2 ) }
+  c.filter_sensitive_data('<CONTENT_NFL_V2_API_KEY>')  { api_key(:content_nfl_v2 ) }
+  c.filter_sensitive_data('<CONTENT_NBA_API_KEY>')  { api_key(:content_nba ) }
+  c.filter_sensitive_data('<CONTENT_NFL_API_KEY>')  { api_key(:content_nfl ) }
+  c.filter_sensitive_data('<ODDS_API_KEY>')  { api_key(:odds ) }
 end
