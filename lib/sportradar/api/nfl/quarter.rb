cb900d91beee05eb29842be0ee27eeb2b68d1637
Tim <timbinous@gmail.com>
2016-10-24 14:36:45 -0400

    getting score from drive

--- lib/sportradar/api/nfl/quarter.rb
+++ lib/sportradar/api/nfl/quarter.rb
@@ -8,9 +8,9 @@ module Sportradar
         @id = data["id"]
         @number = data["number"]
         @sequence = data["sequence"]
-        @home_points = data["home_points"]
+        @home_points = data.dig('scoring', 'home', 'points')
         # @home_points = response['scoring']['home']['points'] # from play_by_play
-        @away_points = data["away_points"]
+        @away_points = data.dig('scoring', 'away', 'points')
         # @away_points = response['scoring']['away']['points'] # from play_by_play
         @drives = parse_into_array(selector: response["play_by_play"]["drive"], klass: Sportradar::Api::Nfl::Drive) if response["play_by_play"] && response["play_by_play"]["drive"]
       end
