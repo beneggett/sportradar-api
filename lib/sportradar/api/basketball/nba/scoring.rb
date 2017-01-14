5bcfe2e22bb7ebb582b316737d19f2d9cca53e80
Paul Hoffer <git@paulhoffer.com>
2017-01-14 00:07:23 -0700

    handle OT for scoring

--- lib/sportradar/api/basketball/nba/scoring.rb
+++ lib/sportradar/api/basketball/nba/scoring.rb
@@ -41,7 +41,8 @@ module Sportradar
             # handle data from team (all quarters)
             # handle data from quarter (both teams)
             # handle data from game?
-            @scores.each { |k, v| v.merge!(new_scores.fetch(k, {})) }
+            @scores.each { |k, v| v.merge!(new_scores.delete(k) || {} ) }
+            new_scores.each { |k, v| @scores.merge!(k => v) }
           end
 
           def points(team_id)
@@ -54,19 +55,27 @@ module Sportradar
           def parse_from_box(data)
             id = data.dig('team', 0, 'id')
             da = data.dig('team', 0, 'scoring', 'quarter')
-            a = [da].compact.flatten(1).each { |h| h[id] = h.delete('points').to_i }
+            da_ot = data.dig('team', 0, 'scoring', 'overtime')
+            arr = [da].compact.flatten(1)
+            a = arr + [da_ot].compact.flatten(1)
+            a.each { |h| h[id] = h.delete('points').to_i }
             id = data.dig('team', 1, 'id')
             da = data.dig('team', 1, 'scoring', 'quarter')
-            b = [da].compact.flatten(1).each { |h| h[id] = h.delete('points').to_i }
-            a.zip(b).map{ |a, b| [a['number'].to_i, a.merge(b)] }.sort{ |(a,_), (b,_)| a <=> b }.to_h
+            da_ot = data.dig('team', 1, 'scoring', 'overtime')
+            arr = [da].compact.flatten(1)
+            b = arr + [da_ot].compact.flatten(1)
+            b.each { |h| h[id] = h.delete('points').to_i }
+            a.zip(b).map{ |a, b| [a['sequence'].to_i, a.merge(b)] }.sort{ |(a,_), (b,_)| a <=> b }.to_h
           rescue => e
             binding.pry
           end
 
           def parse_from_pbp(data)
             quarters = data['quarter'][1..-1]
+            overtimes = data['overtime']
+            overtimes = [overtimes] if overtimes.is_a?(Hash)
             quarters = quarters[0] if quarters.is_a?(Array)
-            data = quarters.map{|q| q['scoring'] }
+            data = (quarters + overtimes).map{|q| q['scoring'] }
             data.map.with_index(1) { |h, i| [i, { h.dig('home', 'id') => h.dig('home', 'points').to_i, h.dig('away', 'id') => h.dig('away', 'points').to_i }] }.to_h
           rescue => e
             {}
