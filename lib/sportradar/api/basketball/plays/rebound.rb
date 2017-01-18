f15a6f644c5013fdfec601ffbec72b596e24ebf0
Paul Hoffer <git@paulhoffer.com>
2017-01-17 17:44:06 -0700

    offensive/defensive rebounds

--- lib/sportradar/api/basketball/plays/rebound.rb
+++ lib/sportradar/api/basketball/plays/rebound.rb
@@ -8,6 +8,12 @@ module Sportradar
         def display_type
           'Rebound'
         end
+        def defensive?
+          @rebound_type == 'defensive'
+        end
+        def offensive?
+          @rebound_type == 'offensive'
+        end
         def parse_statistics(data)
           super
           @rebound_type = @statistics.dig(base_key, 'rebound_type')
