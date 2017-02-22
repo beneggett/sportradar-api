bbd2380f645c017d0bf7a223f4fd4367473ad9fa
Paul Hoffer <git@paulhoffer.com>
2017-02-22 12:35:35 -0700

    handle placeholder tournament teams

--- lib/sportradar/api/basketball/ncaamb/team.rb
+++ lib/sportradar/api/basketball/ncaamb/team.rb
@@ -10,7 +10,11 @@ module Sportradar
               existing.update(data, **opts)
               existing
             else
-              @all_hash[data['id']] = super
+              if data['id']
+                @all_hash[data['id']] = super
+              else # tournament placeholder
+                super
+              end
             end
           end
           def self.all
