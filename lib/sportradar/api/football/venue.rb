8e7a35ff54da3b7b1371b98697e995873c1d2e0f
Paul Hoffer <git@paulhoffer.com>
2017-06-26 08:28:03 -0600

    venue update

--- lib/sportradar/api/football/venue.rb
+++ lib/sportradar/api/football/venue.rb
@@ -2,19 +2,20 @@ module Sportradar
   module Api
     module Football
       class Venue < Data
-        attr_accessor :response, :id, :name, :city, :state, :country, :zip, :address, :capacity, :surface, :roof_type
+        attr_accessor :response, :id, :name, :city, :state, :country, :zip, :address, :capacity, :surface, :roof_type, :timezone
 
         def initialize(data)
-          @response = data
-          @id = data["id"]
-          @name = data["name"]
-          @city = data["city"]
-          @state = data["state"]
-          @country = data["country"]
-          @zip = data["zip"]
-          @address = data["address"]
-          @capacity = data["capacity"]
-          @surface = data["surface"]
+          @response  = data
+          @id        = data["id"]
+          @name      = data["name"]
+          @city      = data["city"]
+          @state     = data["state"]
+          @country   = data["country"]
+          @zip       = data["zip"]
+          @address   = data["address"]
+          @capacity  = data["capacity"]
+          @timezone  = data['timezone']
+          @surface   = data["surface"]
           @roof_type = data["roof_type"]
         end
 
