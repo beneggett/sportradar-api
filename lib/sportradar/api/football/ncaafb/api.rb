1d0a61ca94ff2ac923766ea234e84d7e5430e7f3
Paul Hoffer <git@paulhoffer.com>
2017-08-15 21:14:45 -0600

    fix sim api key hack

--- lib/sportradar/api/football/ncaafb/api.rb
+++ lib/sportradar/api/football/ncaafb/api.rb
@@ -44,7 +44,7 @@ module Sportradar
           end
 
           def api_key
-            if !['t', 'sim-t'].include?(access_level) || (default_access_level == 'rt' && access_level == 'sim')
+            if !['t', 'sim-t'].include?(access_level) || (default_access_level == 'rt' && access_level == 'sim-t')
               ::Sportradar::Api.api_key_params('ncaafb', 'production')
             else
               ::Sportradar::Api.api_key_params('ncaafb')
