8cdd3a3e2c5c1f8cb406b898e190043c5c2a8577
Paul Hoffer <git@paulhoffer.com>
2022-07-10 15:55:32 -0700

    missing file

---
+++ lib/sportradar/api/football/stats_shim.rb
@@ -0,0 +1,24 @@
+module Sportradar
+  module Api
+    module Football
+      class StatsShim
+        attr_reader :game
+
+        def initialize(game)
+          @game = game
+        end
+
+        def dig(player_id, category, stat)
+          game.team_stats.each_value do |stats|
+            player = stats.public_send(category).for_player(player_id)
+            if player
+              return player.send(stat)
+            end
+          end
+          0
+        end
+
+      end
+    end
+  end
+end
